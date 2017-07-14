2d9b7d0230ab172c3fff67116b3d2db65d87c6cb
Vivian Huang <vivianhuang0130@gmail.com>
2017-07-14 12:09:24 -0700

    all routes for album and song

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.json({message: "Record Label API root."})
+  res.json({
+    message: "Record Label API root."
+  })
 })
 
 // THE COMPLETE API:
@@ -24,36 +26,56 @@ app.get('/', (req, res) => {
 ///////////////////////////////////////////////
 
 // get all albums
-app.get('/albums', (req,res)=> {
-  Album.find({},(err, albums)=>{
-    if(err) return console.log(err)
+app.get('/albums', (req, res) => {
+  Album.find({}, (err, albums) => {
+    if (err) return console.log(err)
     res.json(albums)
   })
 })
 
 // post a new album
-app.post('/albums', (req, res)=> {
+app.post('/albums', (req, res) => {
   Album.create(req.body, (err, album) => {
-    if (err)return console.log(err)
+    if (err) return console.log(err)
     res.json(album)
   })
 })
 
 // get a specific album
+app.get('/albums/:id', (req, res) => {
+  Album.findById(req.params.id, (err, specificAlbum) => {
+    if (err) return console.log(err)
+    res.json(specificAlbum)
+  })
+})
 
 // delete an album
-
+app.delete('/albums/:id', (req, res) => {
+  Album.findByIdAndRemove(req.params.id, (err, deleteAlbum) => {
+    if (err) return console.log(err)
+    res.json({
+      message: 'Album deleted!',
+      Album: deleteAlbum
+    })
+  })
+})
 
 // 2. SONG ROUTES:
 ///////////////////////////////////////////////
 
 // get all songs in an album
+app.get('/albums/:id/songs', (req, res) => {
+	Album.findById(req.params.id, (err, album) => {
+		if(err) return console.log(err)
+		res.json(album.songs)
+	})
+})
 
 // post a new song to a specific album
-  //which album?
-  //song details?
-    //push update into array
-    //save it
+//which album?
+//song details?
+//push update into array
+//save it
 app.post('/albums/:id/songs', (req, res) => {
   Album.findById(req.params.id, (err, album) => { //find
     album.songs.push(req.body) //add song to array
@@ -64,10 +86,27 @@ app.post('/albums/:id/songs', (req, res) => {
 })
 
 // get a specific song from a specific album
-
+app.get('/albums/:id/songs/:song_id', (req, res) => {
+  Album.findById(req.params.id, (err, album) => { //find
+    var song = album.songs.id(req.params.song_id) //add song to array
+    res.send(song)
+  })
+})
 // delete a song from an album
-
-
+app.delete('/albums/:id/songs/:song_id', (req, res) => {
+  Album.findById(req.params.id, (err, album) => {
+    var song = album.songs.id(req.params.song_id)
+    if (song !== null) { //we found the song
+      song.remove()
+      album.save(function(err) {
+        if (err) return console.log(err);
+        res.json(album)
+      })
+    }
+    // we didnt find the song
+    else res.send(album)
+  })
+})
 // ARTIST ROUTES
 ///////////////////////////////////////////////
 
