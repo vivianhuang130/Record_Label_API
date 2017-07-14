76ba752c678b2d859bcbabe2a2872526022497fb
Vivian Huang <vivianhuang0130@gmail.com>
2017-07-14 12:17:55 -0700

    Artist model created

--- models/Artist.js
+++ models/Artist.js
@@ -1,1 +1,13 @@
 // Artist model here...
+const mongoose = require('mongoose')
+
+const artistschema = new mongoose.Schema({
+  name: String,
+  albums: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Album' //name of Album model
+  }] //id of this album, is referring to an album
+})
+
+const Artist = mongoose.model('Artist', artistSchema)
+module.exports = Artist
